d8a982291d50ec39598eba436e13120442b06587
Kayne Ruse <kayneruse@gmail.com>
2022-02-07 03:03:01 +1100

    Tweak

--- elements.jsx
+++ elements.jsx
@@ -35,9 +35,9 @@ const Elements = props => {
 			<div className='table noCollapse'>
 				{unshift(Object.keys(elements)).map((offenseKey, offenseIndex) => {
 					return (
-					 	<div className='row' key={offenseIndex}>
-					 		{unshift(elements[offenseKey] ? Object.keys(elements[offenseKey]) : topRow).map((defenceKey, defenceIndex) => {
-					 			//render each "cell" of the table, taking care on the edges too
+						<div className='row' key={offenseIndex}>
+							{unshift(elements[offenseKey] ? Object.keys(elements[offenseKey]) : topRow).map((defenceKey, defenceIndex) => {
+								//render each "cell" of the table, taking care on the edges too
 								if (offenseIndex == 0 && defenceIndex == 0) {
 									return <div className='col' key={defenceIndex} />
 								}
@@ -45,7 +45,7 @@ const Elements = props => {
 								//top row
 								if (offenseIndex == 0) {
 									return (
-										<div className='col' key={defenceIndex}>
+										<div className='col centered middle' key={defenceIndex}>
 											<img alt={defenceKey} src={`/img/icons/100/${defenceKey}-100.png`} style={{maxWidth: '50px'}} />
 										</div>
 									);
@@ -54,7 +54,7 @@ const Elements = props => {
 								//down the side
 								if (defenceIndex == 0) {
 									return (
-										<div className='col' key={defenceIndex}>
+										<div className='col centered middle' key={defenceIndex}>
 											<img alt={offenseKey} src={`/img/icons/100/${offenseKey}-100.png`} style={{maxWidth: '50px'}} />
 										</div>
 									);
@@ -63,16 +63,17 @@ const Elements = props => {
 								//print out the multiplier
 								const style = {
 									paddingTop: '0.5em',
-									backgroundColor: elements[offenseKey][defenceKey] > 1 ? 'green' : elements[offenseKey][defenceKey] < 1 ? 'red' : ''
+									backgroundColor: elements[offenseKey][defenceKey] > 1 ? 'green' : elements[offenseKey][defenceKey] < 1 ? 'red' : '',
+									color: elements[offenseKey][defenceKey] == 1 ? 'black' : 'white',
 								};
 
 								return (
 									<div className='col centered middle' style={style} key={defenceIndex}>
-										<p className='text'>{elements[offenseKey][defenceKey]}</p>
+										<p className='text'>{elements[offenseKey][defenceKey] == 1 ? '-' : elements[offenseKey][defenceKey]}</p>
 									</div>
 								);
 							})}
-					 	</div>
+						</div>
 					);
 				})}
 			</div>
